50e814fab2e26a96b5cd5d79735f3e2a527208d8
Maxim Manylov <manylov@gmail.com>
2023-09-16 00:34:14 +0600

    Update mint.ts

--- scripts/mint.ts
+++ scripts/mint.ts
@@ -5,7 +5,7 @@ import { NetworkProvider, sleep } from '@ton-community/blueprint';
 export async function run(provider: NetworkProvider, args: string[]) {
     const ui = provider.ui();
 
-    const address = Address.parse(args.length > 0 ? args[0] : await ui.input('SimpleCounter address'));
+    const address = Address.parse(args.length > 0 ? args[0] : await ui.input('Address of recepient'));
 
     if (!(await provider.isContractDeployed(address))) {
         ui.write(`Error: Contract at address ${address} is not deployed!`);
